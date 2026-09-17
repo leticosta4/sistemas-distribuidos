bf64cd31c3f9abd97f9a08f3e35b09fdd8540688
Sophia Lima <sophialimasantos4@gmail.com>
2026-09-17 09:54:39 -0300

    feat: base para multiplicação de matrizes sync, async e com middleware

---
+++ mult-matrizes/mult.py
@@ -0,0 +1,13 @@
+# síncrona pra cada processo fazer a multiplicaçaõ de uma linha e retornar
+
+# depois assíncrona
+
+# depois com middleware
+import numpy as np
+
+A = ([0,1,3], [4,3,6], [7,8,9])
+B = ([10,11,12], [13,14,15], [16,17,18])
+
+for i in range(len(A)):
+    pass
+    
